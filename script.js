da205de012c06fe25b495fb9047b211a8b551ea2
Ahmad Ajmi <ahmad@ahmadajmi.com>
2017-06-23 02:23:20 +0200

    Some nice prettier code with Prettier

--- script.js
+++ script.js
@@ -1,63 +1,70 @@
-'use strict';
+"use strict";
 
 let checkBoxes = document.querySelectorAll('input[type="checkbox"]'),
-    checkBoxValues = JSON.parse(localStorage.getItem('checkBoxValues')) || {},
-    checkBoxesCount = document.querySelector('.checkBoxesCount'),
-    checkedBoxesCount = document.querySelector('.checkedBoxesCount'),
-    progress = document.querySelector('.progress-bar'),
-    resetButton = document.querySelector('.reset-button'),
-    checkBoxesLength = checkBoxes.length;
+  checkBoxValues = JSON.parse(localStorage.getItem("checkBoxValues")) || {},
+  checkBoxesCount = document.querySelector(".checkBoxesCount"),
+  checkedBoxesCount = document.querySelector(".checkedBoxesCount"),
+  progress = document.querySelector(".progress-bar"),
+  resetButton = document.querySelector(".reset-button"),
+  checkBoxesLength = checkBoxes.length;
 
 function loadIds() {
   checkBoxes.forEach(element => {
-    let generateId = content => content.toLocaleLowerCase().replace(/[ ,.!?;:'-]/g, '');
-    element.setAttribute('id', generateId(element.parentNode.textContent));
+    let generateId = content =>
+      content.toLocaleLowerCase().replace(/[ ,.!?;:'-]/g, "");
+    element.setAttribute("id", generateId(element.parentNode.textContent));
   });
 }
 
 function loadValues() {
-  let initialCounterValue = localStorage.getItem('checkedBoxesCountFromStorage') || 0,
-      initialCheckedCounterValue = localStorage.getItem('checkedBoxesCountFromStorage') || 0;
+  let initialCounterValue =
+    localStorage.getItem("checkedBoxesCountFromStorage") || 0,
+    initialCheckedCounterValue =
+      localStorage.getItem("checkedBoxesCountFromStorage") || 0;
 
   checkBoxesCount.textContent = checkBoxesLength;
   checkedBoxesCount.textContent = initialCounterValue;
-  progress.style.transform = `scaleX(${initialCheckedCounterValue / checkBoxesLength})`;
+  progress.style.transform = `scaleX(${initialCheckedCounterValue /
+    checkBoxesLength})`;
 
   Object.keys(checkBoxValues).forEach(key => {
-    document.getElementById(key).checked = checkBoxValues[key]
+    document.getElementById(key).checked = checkBoxValues[key];
   });
 }
 
 function updateStorage(element) {
   checkBoxValues[element.id] = element.checked;
-  localStorage.setItem('checkBoxValues', JSON.stringify(checkBoxValues));
+  localStorage.setItem("checkBoxValues", JSON.stringify(checkBoxValues));
 }
 
 function updateCounter() {
   let globalCheckedCounter = 0;
 
-  document.querySelectorAll('input:checked').forEach(() => {
+  document.querySelectorAll("input:checked").forEach(() => {
     globalCheckedCounter += 1;
   });
 
   checkedBoxesCount.textContent = globalCheckedCounter;
-  progress.style.transform = `scaleX(${globalCheckedCounter / checkBoxesLength})`;
-  localStorage.setItem('checkedBoxesCountFromStorage', globalCheckedCounter);
+  progress.style.transform = `scaleX(${globalCheckedCounter /
+    checkBoxesLength})`;
+  localStorage.setItem("checkedBoxesCountFromStorage", globalCheckedCounter);
 
   updateStorage(this);
 }
 
 function resetCheckboxes() {
-  checkBoxes.forEach(element => element.checked = false);
+  checkBoxes.forEach(element => (element.checked = false));
 
   Object.keys(checkBoxValues).forEach(key => delete checkBoxValues[key]);
 
   updateCounter();
 }
 
-checkBoxes.forEach(checkBox => checkBox.addEventListener('click', updateCounter));
+checkBoxes.forEach(checkBox =>
+  checkBox.addEventListener("click", updateCounter)
+);
 
-resetButton.addEventListener('click', resetCheckboxes);
+resetButton.addEventListener("click", resetCheckboxes);
 
-loadIds()
+loadIds();
 loadValues();
